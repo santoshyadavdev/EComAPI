665453ea623ff4af1006fe8234ae29320bfaa27f
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-04-03 00:51:29 +0530

    added user api

--- src/app/controllers/usercontroller.ts
+++ src/app/controllers/usercontroller.ts
@@ -1,17 +1,71 @@
 import { Request, Response, NextFunction, Errback } from "express";
+import { User } from '../models/User';
+import { compareSync } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 
 export class UserController {
+
   static login(req: Request, res: Response, next: NextFunction) {
-    res.json({ user: "Test", success: "true" });
+    const private_key = process.env.PRIVATEKEY || '';
+    User.findOne({ email: req.body.email }, (err: Errback, result: any) {
+      if (err) {
+        res.status(500).json({ status: 'failed', message: err })
+      } else {
+        if (result != undefined) {
+          if (compareSync(req.body.password, result.password)) {
+            const token = sign({ id: result._id }, private_key, { expiresIn: '1h' });
+            res.json({ status: 'success', message: 'Login Success!', data: token })
+          } else {
+            res.json({ status: 'failed', message: 'UserName or Password is incorrect!' })
+          }
+        } else {
+          res.json({ status: 'failed', message: 'UserName or Password is incorrect!' });
+        }
+      }
+    })
   }
 
   static registration(req: Request, res: Response, next: NextFunction) {
-    console.log(req.body);
-    res.json({ user: "Test", success: "true" });
+    const user = new User(req.body);
+    User.create(user, (err: Errback, result: any) => {
+      if (err) {
+        res.status(500).json(
+          { status: 'failed', message: err })
+      } else {
+        res.json({ status: 'success', message: 'Registration Successful!', data: result })
+      }
+
+    })
   }
 
   static updateProfile(req: Request, res: Response, next: NextFunction) {
-    console.log(req.body);
-    res.json({ user: "Test", success: "true" });
+    const userId = req.body.userId;
+
+    User.findByIdAndUpdate(userId, {
+      $set: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        addressInfo: req.body.addressInfo
+      }
+    }, (err: Errback, result: any) => {
+      if (err) {
+        res.status(500).json({ status: 'failed', message: err })
+      } else {
+        res.json({ status: 'success', message: 'Pofile Updated!', data: null })
+      }
+
+    })
+  }
+
+  static getProfile(req: Request, res: Response, next: NextFunction) {
+    const userId = req.body.userId;
+    User.findById(userId, (err: Errback, result: any) => {
+      if (err) {
+        res.status(500).json({ status: 'failed', message: err })
+      } else {
+        res.json({ status: 'success', message: 'Pofile Updated!', data: result })
+      }
+    })
   }
 }
