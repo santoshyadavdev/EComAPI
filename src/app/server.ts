cf6480bb41ecd239b7c0c9ee429eb2f777463a6c
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-04-07 15:31:42 +0530

    api changes

--- src/app/server.ts
+++ src/app/server.ts
@@ -10,6 +10,13 @@ var app = express();
 
 // app.get("/", (req, res) => res.send("This is get express API"));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
